5ecde844fb8677ce755a9df12acb31ee70bfb90b
Namory <namory@Mac.lan>
2025-05-18 13:38:05 +0200

    ajout du logo et de la bar de navigation

--- Maplante/src/app/app.component.ts
+++ Maplante/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from './banner/banner.component';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [BannerComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
